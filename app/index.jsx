8f1064b5cfbd51dc3b9990bc22739984a8f19332
Kannnnng <575664647@qq.com>
2017-04-10 12:40:04 +0800

    添加热替换与容错插件

--- app/index.jsx
+++ app/index.jsx
@@ -6,6 +6,9 @@ class App extends React.Component {
     return (
       <div className='container'>
         <h1>Hello React!</h1>
+        <h1>Hello React!</h1>
+        <h1>Hello React!</h1>
+        <h1>Hello React!</h1>
       </div>
     )
   }
