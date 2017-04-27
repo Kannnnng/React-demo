d5a8b6c68184db24ddcb79c8ac5716359b033248
Kannnnng <575664647@qq.com>
2017-04-27 16:39:00 +0800

    修改以前的代码。

--- app/containers/Profile/index.js
+++ app/containers/Profile/index.js
@@ -3,6 +3,16 @@ import PropTypes from 'prop-types'
 import styles from './index.scss'
 
 class Profile extends React.Component {
+  static propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.string,
+  }
+
+  static defaultProps = {
+    name: '闫守康',
+    age: '23',
+  }
+
   constructor(props) {
     super(props)
 
@@ -10,12 +20,9 @@ class Profile extends React.Component {
       liked: 0,
       hobbies: ['read', 'play games'],
     }
-
-    this.handleOnAddHobby = this.handleOnAddHobby.bind(this)
-    this.handleOnClickliked = this.handleOnClickliked.bind(this)
   }
 
-  handleOnAddHobby() {
+  handleOnAddHobby = () => {
     const hobby = this.state.hobbies
     if (this.hobby.value) {
       hobby.push(this.hobby.value)
@@ -25,31 +32,36 @@ class Profile extends React.Component {
     })
   }
 
-  handleOnClickliked() {
+  handleOnClickliked = () => {
     this.setState({ liked: this.state.liked + 1 })
   }
 
   render() {
+    const {
+      name,
+      age,
+    } = this.props
+
+    const {
+      liked,
+      hobbies,
+    } = this.state
+
     return (
       <div className={styles.profileComponent}>
-        <h1>我的名字叫做{this.props.name}</h1>
-        <h1>我今年{this.props.age}岁了</h1>
+        <h1>我的名字叫做{name}</h1>
+        <h1>我今年{age}岁了</h1>
         <button onClick={this.handleOnClickliked}>给我点赞</button>
-        <h2>获得的赞：{this.state.liked}</h2>
+        <h2>获得的赞：{liked}</h2>
         <h2>我的爱好</h2>
         <ul>
-          {this.state.hobbies.map((value, index) => <li key={index}>{value}</li>)}
+          {hobbies.map((value, index) => <li key={index}>{value}</li>)}
         </ul>
         <input type="text" ref={(node) => { this.hobby = node }} />
         <button onClick={this.handleOnAddHobby}>添加爱好</button>
       </div>
     )
   }
-}
-
-Profile.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.string,
 }
 
 export default Profile
