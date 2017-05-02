8eac120f15c6c0f5b06e1d21763fa6cd70986578
Kannnnng <575664647@qq.com>
2017-05-02 22:48:32 +0800

    对配置文件作了一些修改，并增加了些许注释。

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,49 @@
 var path = require('path')  // eslint-disable-line
 var webpack = require('webpack')  // eslint-disable-line
-
 var ROOT_PATH = path.resolve(__dirname)  // eslint-disable-line
 var APP_PATH = path.resolve(ROOT_PATH, 'app')  // eslint-disable-line
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build')  // eslint-disable-line
 var SplitByPathPlugin = require('webpack-split-by-path')  // eslint-disable-line
 
+var entry = {}  // eslint-disable-line
+var output = {}  // eslint-disable-line
+var cache = undefined  // eslint-disable-line
 var plugins = []  // eslint-disable-line
 
-if (process.env === 'pro') {
-  plugins.push(
-    new webpack.optimize.DedupePlugin(),
+/* 如果当前环境是生产环境，就配置一些特定的插件，优化生产环境下的代码 */
+if (process.argv[process.argv.length - 1].slice(6, 9) === 'pro') {
+  entry = {
+    'app': ['babel-polyfill', path.resolve(APP_PATH, 'index.js')],  // eslint-disable-line
+  }
+  output = {
+    path: BUILD_PATH,
+    filename: '[name].[hash].js',
+    chunkFilename: '[name].[hash].js',
+  }
+  cache = false
+  plugins = [
+    /* 去除重复的依赖包的代码，取而代之的是运行的时候请求一个封装函数 */
+    /* 在 webpack2.0 中已不需要 */
+    // new webpack.optimize.DedupePlugin(),
+    /* 根据 id 的使用频率和分布来得出最短的 id 分配给使用频率高的模块 */
+    /* 在 webpack2.0 中已经不需要特别声明 */
+    // new webpack.optimize.OccurenceOrderPlugin(),
+    /* 声明此时的环境是生产环境，这样能够避免开发环境中的一些错误或警告显示代码被打包到文件中 */
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"production"',
     }),
+    /* 压缩 JS 文件， */
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-        /* 消除压缩代码时的报警信息 */
+        /* 消除产生警告的代码，此类代码多来自于引用的模块内部 */
         warnings: false,
       },
     }),
-    new webpack.NoErrorsPlugin(),
+    /* 尽量合并代码 */
+    new webpack.optimize.AggressiveMergingPlugin(),
+    /* 允许错误不打断程序的执行，这在生产环境中很重要 */
+    new webpack.NoEmitOnErrorsPlugin(),
+    /* 按照路径的不同来分割代码 */
     new SplitByPathPlugin([
       {
         name: 'vendor',
@@ -29,19 +52,30 @@ if (process.env === 'pro') {
     ], {
       manifest: 'app-entry',
     }),
-  )
-}
-
-module.exports = {
-  entry: {
-    babel: 'babel-polyfill',
-    app: path.resolve(APP_PATH, 'index.js'),
-  },
-  output: {
+  ]
+} else {
+  entry = {
+    'app': ['webpack/hot/dev-server', 'babel-polyfill', path.resolve(APP_PATH, 'index.js')],  // eslint-disable-line
+  }
+  output = {
     path: BUILD_PATH,
     filename: '[name].js',
     chunkFilename: '[name].js',
-  },
+  }
+  cache = true
+  plugins = [
+    new webpack.HotModuleReplacementPlugin(),
+  ]
+}
+
+module.exports = {
+  /* 入口文件 */
+  entry,
+  /* 出口文件 */
+  output,
+  /* 设置缓存是否开启，当前设置是在开发环境下缓存开启 */
+  cache,
+  /* 源代码与编译后代码的匹配模式 */
   // devtool: 'cheap-eval-source-map',
   devtool: 'cheap-module-eval-source-map',
   devServer: {
@@ -57,6 +91,7 @@ module.exports = {
       },
     },
   },
+  /* 针对不同的文件类型配置不同的 loader，并设置对应的配置项 */
   module: {
     loaders: [
       {
@@ -104,7 +139,9 @@ module.exports = {
     ],
   },
   resolve: {
+    /* 可能用到的文件扩展名 */
     extensions: ['.js', '.jsx', 'css', 'scss'],
+    /* 文件路径别名，方便在写代码时对模块的引用 */
     alias: {
       actions: path.resolve(APP_PATH, 'actions'),
       components: path.resolve(APP_PATH, 'components'),
