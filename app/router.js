1411c08714eb05ba67e73dca87dda0f7dddc3f73
Kannnnng <575664647@qq.com>
2017-05-17 17:17:20 +0800

    修改代码
    删掉了 react-router 依赖包，只需要 react-router-dom 依赖包就可以了；
    添加了页面的重定向功能。

--- app/router.js
+++ app/router.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 import Profile from 'containers/Profile' // 引入各容器组件
 import Test from 'containers/Test' // 引入各容器组件
 
 const routes = (
   <div>
-    <Route path="/test" component={Test} />
-    <Route path="/app" component={Profile} />
+    <Route path='/test' component={Test} />
+    <Route path='/app' component={Profile} />
+    <Redirect from='/' to='app' />
   </div>
 )
 
