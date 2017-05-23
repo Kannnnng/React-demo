e4b1e6e769c6569408166c314c99f60a91d31d36
Kannnnng <575664647@qq.com>
2017-05-23 17:38:34 +0800

    提交代码

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -2,16 +2,11 @@ import React from 'react'
 import styles from './index.scss'
 
 class Home extends React.Component {
-  myTest = () => {
-    console.log('测试')
-  }
-
   render() {
     return (
       <div className={styles.container}>
         <div className={styles.title}>
           {'这是一个标题刷新'}
-          <button onClick={this.myTest}>{'点我查看'}</button>
         </div>
       </div>
     )
