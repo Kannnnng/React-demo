971c60b7445d5636d839fac7ad147024d2f3dc31
Kannnnng <575664647@qq.com>
2017-05-25 18:02:50 +0800

    提交代码

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -2,16 +2,14 @@ import React from 'react'
 import styles from './index.scss'
 
 class Home extends React.Component {
-  handleOnClick = () => {
-    console.log('我要热更新啦啦啦啦啦')
-  }
-
   render() {
     return (
       <div className={styles.container}>
-        <div>快更新</div>
-        <input type="button" value="点我查看效果" onClick={this.handleOnClick} />
-        <div>你特么倒是更新啊</div>
+        <div className={styles.centerBox}>
+          <div className={styles.title}>
+            Einskang
+          </div>
+        </div>
       </div>
     )
   }
