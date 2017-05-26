d6c98e23535201ee943fb2df8be9e84271cfa616
Kannnnng <575664647@qq.com>
2017-05-26 17:34:29 +0800

    把主页做好了，嘿嘿

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -1,21 +1,108 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import {
+  AppBar,
+  Drawer,
+  List,
+  ListItem,
+  Snackbar,
+} from 'material-ui'
 import styles from './index.scss'
 
 class Home extends React.Component {
+  static propTypes = {
+    history: PropTypes.object,
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      openCatalog: false,
+      openSnackbar: false,
+      snackbarMessage: '',
+    }
+  }
+
+  handleOnClickCatalog = () => {
+    this.setState({ openCatalog: !this.state.openCatalog })
+  }
+
+  handleOnClickCatalogLeftIcon = () => {
+    this.setState({
+      openSnackbar: true,
+      snackbarMessage: '直接选下面的就可以啦',
+    })
+  }
+
+  handleOnClickCatalogList = (data) => () => {
+    this.props.history.push(data)
+  }
+
+  handleOnCloseSnackbar = () => {
+    this.setState({
+      openSnackbar: false,
+      snackbarMessage: '',
+    })
+  }
+
   render() {
+    const {
+      openCatalog,
+      openSnackbar,
+      snackbarMessage,
+    } = this.state
+
     return (
-      <div className={styles.container}>
+      <div className={`${styles.container} animated bounceInDown`}>
         <div className={styles.centerBox}>
-          <div className={styles.title}>
-            <div style={{ textAlign: 'center' }}>
-              <Link to="/test">跳转至测试页</Link>
-            </div>
-            <div style={{ textAlign: 'center' }}>
-              <Link to="/pro">跳转至个人主页</Link>
-            </div>
+          <div className={styles.avatar} />
+          <div
+            className={styles.title}
+          >
+            <span
+              className={styles.titleText}
+              onTouchTap={this.handleOnClickCatalog}
+            >
+              Einskang
+            </span>
+          </div>
+          <div className={styles.titleUnderLine} />
+          <div className={styles.introduction}>
+            这里是我的个人博客
           </div>
         </div>
+        <Drawer
+          containerClassName={styles.catalog}
+          open={openCatalog}
+          openSecondary
+        >
+          <AppBar
+            title="目录"
+            onLeftIconButtonTouchTap={this.handleOnClickCatalogLeftIcon}
+          />
+          <List>
+            <ListItem
+              primaryText="跳转到测试页"
+              onTouchTap={this.handleOnClickCatalogList('/test')}
+            />
+            <ListItem
+              primaryText="跳转到个人页"
+              onTouchTap={this.handleOnClickCatalogList('/pro')}
+            />
+            <ListItem
+              primaryText="跳转到我的博客"
+              onTouchTap={() => { window.location = 'https://kannnnng.github.io/Blog/' }}
+            />
+          </List>
+        </Drawer>
+        <Snackbar
+          autoHideDuration={2000}
+          contentStyle={{ textAlign: 'center' }}
+          message={snackbarMessage}
+          onRequestClose={this.handleOnCloseSnackbar}
+          open={openSnackbar}
+        />
       </div>
     )
   }
