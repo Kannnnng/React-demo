5d84815aac1c21e2f94e8b91a7369511e29c9ba4
Kannnnng <575664647@qq.com>
2017-08-06 13:42:45 +0800

    继续修改项目结构

---
+++ app/app.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
+import routes from './router'
+
+const Router = process.env.NODE_ENV === 'production' ? HashRouter : BrowserRouter
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router basename="/">
+        {routes}
+      </Router>
+    )
+  }
+}
+
+export default App
--- app/index.js
+++ app/index.js
@@ -1,24 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import { AppContainer } from 'react-hot-loader'
+import { MuiThemeProvider } from 'material-ui'
+import { getMuiTheme } from 'material-ui/styles'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import App from 'containers/App'
+import App from './app'
+import configStore from './store'
 
-import './index.css'
+import './styles'
 
+/* material-ui Tap Event Bug */
 injectTapEventPlugin()
+
+/* 创建 store */
+const initialState = {}
+const store = configStore(initialState)
+
+/* material-ui 所需属性 */
+const muiTheme = getMuiTheme({})
 
 if (process.env.NODE_ENV === 'production') {
   ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <App />
+      </MuiThemeProvider>
+    </Provider>,
     document.getElementById('app')  // eslint-disable-line
   )
 } else {
   const render = (Component) => {
     ReactDOM.render(
-      <AppContainer>
-        <Component />
-      </AppContainer>,
+      <Provider store={store}>
+        <MuiThemeProvider muiTheme={muiTheme}>
+          <AppContainer>
+            <Component />
+          </AppContainer>
+        </MuiThemeProvider>
+      </Provider>,
       document.getElementById('app')  // eslint-disable-line
     )
   }
@@ -26,7 +46,7 @@ if (process.env.NODE_ENV === 'production') {
   render(App)
 
   if (module.hot) {
-    module.hot.accept('containers/App', () => {
+    module.hot.accept('./app', () => {
       render(App)
     })
   }
