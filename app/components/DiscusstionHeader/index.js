16538614d99e9bf7333c33ee4b320a0c4684e7f6
Kannnnng <575664647@qq.com>
2017-08-30 10:08:32 +0800

    提交代码
    1.修改了两个模版的小问题；
    2.完成了讨论顶部组件。

--- app/components/DiscusstionHeader/index.js
+++ app/components/DiscusstionHeader/index.js
@@ -9,68 +9,76 @@ import PropTypes from 'prop-types'
 import Toggle from 'material-ui/Toggle'
 import styles from './styles'
 
-export default class DiscusstionHeader extends React.PureComponent {
-  static propTypes = {
-    data: PropTypes.object,
-  }
+export default function DiscusstionHeader({
+  title,
+  checkedTab,
+  isDiscusstionOpening,
+  handleOnToggleDiscusstion,  // 第二个参数代表当前的开关状态
+  handleOnGoBack,
+}) {
+  const discusstionToggleClassName = `${styles.showTypeButton} ${styles.discusstionToggle}`
+  const speakContentClassName = `${styles.showTypeButton}${checkedTab === 'speakContent' ? ` ${styles.showTypeButtonChecked}` : ''}`  // eslint-disable-line
+  const keyWordClassName = `${styles.showTypeButton}${checkedTab === 'keyWord' ? ` ${styles.showTypeButtonChecked}` : ''}`  // eslint-disable-line
+  const picsClassName = `${styles.showTypeButton}${checkedTab === 'Pics' ? ` ${styles.showTypeButtonChecked}` : ''}`
 
-  static defaultProps = {
-    data: {},
-  }
-
-  state = {
-    stateData: {},
-  }
-
-  render() {
-    const {
-      data,
-    } = this.props
-    const {
-      stateData,
-    } = this.state
-    const discusstionToggleClassName = `${styles.showTypeButton} ${styles.discusstionToggle}`
-
-    return (
-      <div className={styles.container}>
-        <div className={styles.leftArea}>
-          <div className={styles.goBack}>
-            <button>
-              <span />
-            </button>
-          </div>
-          <div className={styles.title}>
-            你是谁的谁你是谁的谁你是谁的谁
-          </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.leftArea}>
+        <div className={styles.goBack}>
+          <button onClick={handleOnGoBack}>
+            <span />
+          </button>
         </div>
-        <div className={styles.rightArea}>
-          <div className={styles.showTypeButton}>
-            <button>
-              <span className={styles.speakContentIcon} />
-              <span className={styles.typeText}>{'发言内容'}</span>
-            </button>
-          </div>
-          <div className={styles.showTypeButton}>
-            <button>
-              <span className={styles.keyWordIcon} />
-              <span className={styles.typeText}>{'核心词'}</span>
-            </button>
-          </div>
-          <div className={styles.showTypeButton}>
-            <button>
-              <span className={styles.PicsIcon} />
-              <span className={styles.typeText}>{'图片墙'}</span>
-            </button>
-          </div>
-          <div className={discusstionToggleClassName}>
-            <Toggle
-              thumbStyle={{ backgroundColor: '#4db553' }}
-              thumbSwitchedStyle={{ backgroundColor: '#3B9E46' }}
-            />
-            <span className={styles.typeText}>{'图片墙'}</span>
-          </div>
+        <div className={styles.title}>
+          {title}
         </div>
       </div>
-    )
-  }
+      <div className={styles.rightArea}>
+        <div className={speakContentClassName}>
+          <button>
+            <span className={styles.speakContentIcon} />
+            <span className={styles.typeText}>{'发言内容'}</span>
+          </button>
+        </div>
+        <div className={keyWordClassName}>
+          <button>
+            <span className={styles.keyWordIcon} />
+            <span className={styles.typeText}>{'核心词'}</span>
+          </button>
+        </div>
+        <div className={picsClassName}>
+          <button>
+            <span className={styles.picsIcon} />
+            <span className={styles.typeText}>{'图片墙'}</span>
+          </button>
+        </div>
+        <div className={discusstionToggleClassName}>
+          <Toggle
+            defaultToggled={isDiscusstionOpening}
+            thumbStyle={{ backgroundColor: '#4DB553' }}
+            thumbSwitchedStyle={{ backgroundColor: '#4DB553' }}
+            trackSwitchedStyle={{ backgroundColor: '#3B9E46' }}
+            onToggle={handleOnToggleDiscusstion}
+          />
+          <span className={styles.typeText}>{isDiscusstionOpening ? '讨论开启中' : '讨论关闭'}</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+DiscusstionHeader.propTypes = {
+  title: PropTypes.string,
+  checkedTab: PropTypes.oneOf(['speakContent', 'keyWord', 'Pics']),
+  isDiscusstionOpening: PropTypes.bool,
+  handleOnToggleDiscusstion: PropTypes.func,
+  handleOnGoBack: PropTypes.func,
+}
+
+DiscusstionHeader.defaultProps = {
+  title: '',
+  checkedTab: 'speakContent',
+  isDiscusstionOpening: false,
+  handleOnToggleDiscusstion: () => {},
+  handleOnGoBack: () => {},
 }
