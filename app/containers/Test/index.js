dfdd597624eaae53790f727d53f2a2df278b3098
Kannnnng <575664647@qq.com>
2017-11-06 16:57:24 +0800

    探索 immutable 数据结构对 react 组件渲染机制的影响

--- app/containers/Test/index.js
+++ app/containers/Test/index.js
@@ -2,36 +2,53 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import { fromJS } from 'immutable'
 import RaisedButton from 'material-ui/RaisedButton'
 import FontIcon from 'material-ui/FontIcon'
 import * as acts from './actions'
 import styles from './styles'
 
-class Test extends React.Component {
+class Test extends React.PureComponent {
   static propTypes = {
     actions: PropTypes.object,
     message: PropTypes.string,
+  }
+
+  state = {
+    data: fromJS({
+      a: 1,
+    }),
   }
 
   handleOnClick = () => {
     this.props.actions.testAction(12345)
   }
 
+  handleOnClickTestButton = () => {
+    this.setState({
+      data: this.state.data.update('a', (a) => a),
+    })
+  }
+
   render() {
     const {
       message,
     } = this.props
-
+    const {
+      data,
+    } = this.state
+    console.log(data.toJS(), 123)
 
     return (
       <div className={styles.divTest}>
         {'这是一段测试文本，点击按钮以后下面显示信息会发生变化'}
         <div>{message}</div>
+        <div>{data.get('a')}</div>
         <div>
           <RaisedButton
             icon={<FontIcon className='fa fa-heart' />}
             label='点击我就会发送 action！'
-            onTouchTap={this.handleOnClick}
+            onTouchTap={this.handleOnClickTestButton}
             primary
           />
         </div>
